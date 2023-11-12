905848c934a7504d534269d3bc7641ad636002ae
Yesssssman <79469058+Yesssssman@users.noreply.github.com>
2023-11-12 21:34:18 +0900

    20.5.17

--- src/main/java/yesman/epicfight/client/input/EpicFightKeyMappings.java
+++ src/main/java/yesman/epicfight/client/input/EpicFightKeyMappings.java
@@ -15,8 +15,8 @@ public class EpicFightKeyMappings {
 	public static final KeyMapping SWITCH_MODE = new KeyMapping("key." + EpicFightMod.MODID + ".switch_mode", InputConstants.KEY_R, "key." + EpicFightMod.MODID + ".combat");
 	public static final KeyMapping DODGE = new KeyMapping("key." + EpicFightMod.MODID + ".dodge", InputConstants.KEY_LALT, "key." + EpicFightMod.MODID + ".combat");
 	public static final KeyMapping GUARD = new KeyMapping("key." + EpicFightMod.MODID + ".guard", InputConstants.KEY_LALT, "key." + EpicFightMod.MODID + ".combat");
-	public static final KeyMapping ATTACK = new  KeyMapping("key." + EpicFightMod.MODID + ".attack", InputConstants.Type.MOUSE, 0, "key." + EpicFightMod.MODID + ".combat");
-	public static final KeyMapping WEAPON_INNATE_SKILL = new WeaponInnateSkillKeyMapping("key." + EpicFightMod.MODID + ".weapon_innate_skill", InputConstants.Type.MOUSE, 0, "key." + EpicFightMod.MODID + ".combat");
+	public static final KeyMapping ATTACK = new  CombatKeyMapping("key." + EpicFightMod.MODID + ".attack", InputConstants.Type.MOUSE, 0, "key." + EpicFightMod.MODID + ".combat");
+	public static final KeyMapping WEAPON_INNATE_SKILL = new CombatKeyMapping("key." + EpicFightMod.MODID + ".weapon_innate_skill", InputConstants.Type.MOUSE, 0, "key." + EpicFightMod.MODID + ".combat");
 	public static final KeyMapping MOVER_SKILL = new  KeyMapping("key." + EpicFightMod.MODID + ".mover_skill", InputConstants.Type.KEYSYM, InputConstants.KEY_SPACE, "key." + EpicFightMod.MODID + ".combat");
 	public static final KeyMapping SKILL_EDIT = new KeyMapping("key." + EpicFightMod.MODID + ".skill_gui", InputConstants.KEY_K, "key." + EpicFightMod.MODID + ".gui");
 	public static final KeyMapping LOCK_ON = new KeyMapping("key." + EpicFightMod.MODID + ".lock_on", InputConstants.KEY_G, "key." + EpicFightMod.MODID + ".combat");
