db238b1fe00090f66ff38d4444035d1a0c848535
Yesssssman <79469058+Yesssssman@users.noreply.github.com>
2023-11-12 21:30:33 +0900

    18.5.17

--- src/main/java/yesman/epicfight/client/input/CombatKeyMapping.java
+++ src/main/java/yesman/epicfight/client/input/CombatKeyMapping.java
@@ -3,18 +3,18 @@ package yesman.epicfight.client.input;
 import com.mojang.blaze3d.platform.InputConstants;
 
 import net.minecraft.client.KeyMapping;
-import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import yesman.epicfight.client.ClientEngine;
 
 @OnlyIn(Dist.CLIENT)
-public class WeaponInnateSkillKeyMapping extends KeyMapping {
-	public WeaponInnateSkillKeyMapping(String description, InputConstants.Type type, int code, String category) {
+public class CombatKeyMapping extends KeyMapping {
+	public CombatKeyMapping(String description, InputConstants.Type type, int code, String category) {
 		super(description, type, code, category);
 	}
 	
 	@Override
 	public boolean isActiveAndMatches(InputConstants.Key keyCode) {
-        return super.isActiveAndMatches(keyCode) && !keyCode.equals(Minecraft.getInstance().options.keyAttack.getKey());
+        return super.isActiveAndMatches(keyCode) && ClientEngine.getInstance().isBattleMode();
     }
 }
