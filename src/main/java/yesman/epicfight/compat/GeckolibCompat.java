581ef72a9474376c467fed3ab084fe267cffd2d0
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-12-03 19:11:39 -0500

    Switch Geckolib using DistExecutor wen it should be FMLEnvironment.dist.isClient()

--- src/main/java/yesman/epicfight/compat/GeckolibCompat.java
+++ src/main/java/yesman/epicfight/compat/GeckolibCompat.java
@@ -1,23 +1,22 @@
 package yesman.epicfight.compat;
 
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 import yesman.epicfight.api.client.model.armor.CustomModelBakery;
 import yesman.epicfight.api.client.model.armor.GeoArmor;
 
 public class GeckolibCompat implements ICompatModule {
 	@Override
 	public void onModEventBus(IEventBus eventBus) {
-		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> ()-> {
+		if (FMLEnvironment.dist.isClient()) {
 			CustomModelBakery.registerNewTransformer(new GeoArmor());
-		});
+		};
 	}
 	
 	@Override
 	public void onForgeEventBus(IEventBus eventBus) {
-		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> ()-> {
+		if (FMLEnvironment.dist.isClient()) {
 			eventBus.addListener(GeoArmor::getGeoArmorTexturePath);
-		});
+		};
 	}
 }
